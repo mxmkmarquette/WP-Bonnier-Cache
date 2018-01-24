2b687573364b3095a4d13b74d0569a23b025f8c3
Alf <alfhenderson@gmail.com>
2018-01-24 12:15:48 +0000

    Apply fixes from StyleCI

--- src/Bonnier/WP/Cache/Admin/PostMetaBox.php
+++ src/Bonnier/WP/Cache/Admin/PostMetaBox.php
@@ -15,23 +15,25 @@ class PostMetaBox
 {
     const MANUEL_TRIGGER_BUTTON = 'wp_bonnier_cache_meta_box_manuel_trigger_btn';
 
-    public static function register() {
-        add_action('do_meta_boxes', function(){
+    public static function register()
+    {
+        add_action('do_meta_boxes', function () {
             add_meta_box('bp_wp_bonnier_cache', 'Bonnier Cache', [__CLASS__, 'meta_box_content'], null, 'side');
         });
         add_action('save_post', [__CLASS__, 'save_meta_box_settings']);
-
     }
 
-    public static function meta_box_content($post) {
+    public static function meta_box_content($post)
+    {
         $status = CacheApi::post(CacheApi::CACHE_STATUS, get_permalink($post->ID), true);
         static::printClearTime('CloudFlare', $status->cloudflare_called_at ?? null);
         static::printClearTime('Facebook', $status->facebook_called_at ?? null);
         static::printClearTime('Cxense', $status->cxense_called_at ?? null);
         static::printManuelTriggerButton();
     }
-    public static function save_meta_box_settings($post) {
-        if(isset($_POST[static::MANUEL_TRIGGER_BUTTON])) {
+    public static function save_meta_box_settings($post)
+    {
+        if (isset($_POST[static::MANUEL_TRIGGER_BUTTON])) {
             CacheApi::post(CacheApi::CACHE_UPDATE, get_permalink($post->ID), true);
         }
     }
@@ -39,12 +41,12 @@ class PostMetaBox
     private static function printClearTime($cacheVendor, $clearTime)
     {
         $clearString = $clearTime ? Carbon::parse($clearTime)->diffForHumans() : 'Not cleared yet';
-        printf("<strong>%s:</strong> %s <br>", $cacheVendor,  $clearString);
+        printf("<strong>%s:</strong> %s <br>", $cacheVendor, $clearString);
     }
 
     private static function printManuelTriggerButton()
     {
-        printf (
+        printf(
             '<br>
             <input class="button-secondary" type="submit" name="%s" value="Clear cache">
             <input class="button-secondary" type="button" onclick="window.location.reload()" value="Refresh">
