3f4cb4fcaefe8897052180ed874e588769c6312b
Magnus Flor Johansen <magnus@m-flor.dk>
2018-08-13 15:23:37 +0200

    WILL-840 - Willow Backend release

--- wp-bonnier-cache.php
+++ wp-bonnier-cache.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Bonnier Cache
  * Plugin URI: http://bonnierpublications.com
  * Description: Bonnier Cache Plugin
- * Version: 1.2.8
+ * Version: 2.0.0
  * Author: Magnus Flor
  * Author URI: http://bonnierpublications.com
  */
